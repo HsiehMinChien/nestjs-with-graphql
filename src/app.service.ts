5ef36f37e89ce634edf5bb82cb57b5a704e1f0cd
MC <mc@MCde-MacBook-Air.local>
2020-03-15 00:32:17 +0800

    Modify default page

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,19 @@ import { Injectable } from "@nestjs/common";
 @Injectable()
 export class AppService {
   getHomePage(): string {
-    return "GraphQL on Nest.js";
+    return `
+    <div style="padding: 15%; text-align:center;">
+      <div style="background-color: #eee; border-radius: 10px; padding: 10px 20px; box-shadow: 2px 2px 2px 1px rgba(0, 0, 0, 0.2);">
+        <h1>Welcome to my nest.js demo project</h1>
+        <h2>NestJs + Typescript + GraphQL(Server side)</h2>
+
+        <br/><br/>
+
+        <a href="/graphql">
+          <h3>Try GraphQL Playground</h3>
+        </a>
+      </div>
+    </div>
+    `;
   }
 }
